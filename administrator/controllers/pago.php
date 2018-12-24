b753629079930c67b54fab42d6fcc7dca03ab746
Ulises <aldouli6@gmail.com>
2018-12-23 20:00:01 -0600

    el pago guarda los datos de compra

--- administrator/controllers/pago.php
+++ administrator/controllers/pago.php
@@ -29,4 +29,53 @@ class Servin2ControllerPago extends JControllerForm
 		$this->view_list = 'pagos';
 		parent::__construct();
 	}
+	public function save(){  
+		//Obtener los objetos de la vista
+	    $mainframe = JFactory::getApplication();
+	    $datos = new Jregistry($mainframe -> input ->get('jform', '', 'array') );
+	    $datos=$datos;
+		//$print=print_r( $datos, true);
+		//$mainframe->enqueueMessage ($print,'notice' );
+		$db = JFactory::getDbo();
+		switch ($datos['tipo']) {
+			case '1':
+
+				break;
+			case '2':
+				$sql="update `#__servin_compras2` SET `abonado` = abonado + ".$datos['pago']." WHERE id =".$datos['compra'].";";
+			    $db->setQuery($sql);
+		 	    $db->execute();
+				$sql="select total, abonado from  `#__servin_compras2` WHERE id =".$datos['compra'].";";
+				$db->setQuery($sql);
+				$result = $db->loadAssoc();					
+				if($result['total']<=$result['abonado']){
+					$sql="update `#__servin_compras2` SET `pagada` = 1 WHERE id =".$datos['compra'].";";
+				    $db->setQuery($sql);
+			 	    $db->execute();
+				}
+				$this->setRedirect('index.php?option=com_servin2&view=pagos');
+				break;
+			case '3':
+				# code...
+				break;
+			default:
+				# code...
+				break;
+		}
+		// if($datos['id']>0){
+		// 	
+		// 	$query='select id, ordering, state, checked_out, checked_out_time,created_by,modified_by,rfc,razon_social,calle,no_ext,no_int,no_int,cp,colonia,ciudad,municipio,estado,email from #__condo_dir_factur WHERE id ='.$datos['id'].';';
+		// 	$db->setQuery($query);
+		// 	$result = $db->loadAssoc();	
+		// 	/*$print=print_r( $result, true);
+		// 	$mainframe->enqueueMessage ($print,'notice' );*/
+		// 	unset($datos['actualizado']);
+		// 	if($result!=$datos){
+		// 		$sql="update `#__condo_dir_factur` SET `actualizado` = 0 WHERE id =".$datos['id'].";";
+		// 	    $db->setQuery($sql);
+		// 	    $db->execute();
+		// 	}
+		// }	
+		parent::save();   	
+	}
 }
--- administrator/views/pago/tmpl/edit.php
+++ administrator/views/pago/tmpl/edit.php
@@ -22,7 +22,73 @@ $document->addStyleSheet(JUri::root() . 'media/com_servin2/css/form.css');
 <script type="text/javascript">
 	js = jQuery.noConflict();
 	js(document).ready(function () {
-		
+	if (!'<?php print_r($this->item->consignacion) ?>') {		
+		bloquearchzc('#jform_consignacion');	
+	}
+
+	function bloquearchzc(cadena){
+ 		js(cadena+'_chzn').addClass('chzn-disabled');
+	    js(cadena).attr('disabled','disabled');	    	
+ 		js(cadena).trigger("liszt:updated");
+ 	}
+	function desbloquearchzc(cadena){
+ 		js(cadena+'_chzn').removeClass('chzn-disabled');
+	    js(cadena).removeAttr('disabled');    	
+ 		js(cadena).trigger("liszt:updated");
+ 	}
+ 	function llenarconsigpermitidas(value){
+ 		js.ajax({ 
+	            url: "index.php?option=com_servin2&task=consigpermitidas&view=ajaxs&tmpl=ajax&id=" + value,  
+	            async: true, 
+	            success: function(result){
+	            var obj = result;
+				objeto = JSON.parse(obj);
+            	console.log(objeto); 
+            	js('#jform_consignacion').empty();
+            	js("#jform_consignacion").append('<option abonado="" value="">Selecciona una opción</option>');
+            	js.each( objeto, function( key, value ) {
+            		var s = (value['id']=="<?php print_r($this->item->consignacion) ?>")?'selected="selected"':"";
+            		js("#jform_consignacion").append('<option '+s+' abonado="'+value['abono']+'" value="'+value['id']+'">'+value['descripcion']+'</option>');
+				});
+				js("#jform_consignacion").trigger("liszt:updated");
+
+            	},
+	            error: function(result) {
+	                console.log(result);
+	            }
+	    	});
+ 	}		
+ 	function clearselects(){
+ 		js('#jform_compra').prop("selectedIndex",-1); 
+ 		js('#jform_venta').prop("selectedIndex",-1); 
+ 		js('#jform_consignacion').prop("selectedIndex",-1); 
+ 		js("label[for='jform_tipo_consignacion1']").removeClass('active btn-success');
+ 		js("label[for='jform_tipo_consignacion0']").removeClass('active btn-danger');
+ 		js('input:radio[name="jform[tipo_consignacion]"][value=0]').removeAttr('checked');
+ 		js('input:radio[name="jform[tipo_consignacion]"][value=1]').removeAttr('checked');
+ 		js("#jform_compra").trigger("liszt:updated");
+ 		js("#jform_venta").trigger("liszt:updated");
+ 		js("#jform_consignacion").trigger("liszt:updated");
+ 		bloquearchzc('#jform_consignacion');
+ 	}
+	js('#jform_tipo_consignacion').change(function() {
+		var radio=js("#jform_tipo_consignacion input[type='radio']:checked").val();
+		desbloquearchzc('#jform_consignacion');
+		llenarconsigpermitidas(radio);
+	});
+
+	js('.comprasventas').change(function() {
+		var cadena1 = js(this).find(":selected").text();
+		var cadena2 = cadena1.slice(cadena1.lastIndexOf("|")+2, cadena1.length);
+		js('#jform_pago').attr("max", cadena2 );
+		js('#lbldatos').text('Solo se pueden pagar '+cadena2+' pesos')
+		alert(cadena2);
+	});
+
+	js('#jform_tipo').change(function() {
+		clearselects();
+
+	});
 	js('input:hidden.compra').each(function(){
 		var name = js(this).attr('name');
 		if(name.indexOf('comprahidden')){
@@ -108,7 +174,15 @@ $document->addStyleSheet(JUri::root() . 'media/com_servin2/css/form.css');
 						echo '<input type="hidden" class="venta" name="jform[ventahidden]['.$value.']" value="'.$value.'" />';
 					endif;
 				endforeach;
-			?>				<?php echo $this->form->renderField('pago'); ?>
+			?>
+			<div class="control-group"> 
+			<div class="control-label">
+			</div>
+				<div class="controls">
+					<label id="lbldatos"></label>
+				</div>
+			</div>
+							<?php echo $this->form->renderField('pago'); ?>
 				<?php echo $this->form->renderField('metodo'); ?>
 				<?php echo $this->form->renderField('datos_pago'); ?>
 				<?php echo $this->form->renderField('fecha'); ?>
